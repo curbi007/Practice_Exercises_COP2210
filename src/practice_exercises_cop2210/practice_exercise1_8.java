64d8ac50ecd32a1edd20125a897d64ccd85c7bb3
curbi007 <curbi007@fiu.edu>
2016-01-16 13:41:29 -0500

    Update practice_exercise1_8.java

--- src/practice_exercises_cop2210/practice_exercise1_8.java
+++ src/practice_exercises_cop2210/practice_exercise1_8.java
@@ -9,6 +9,21 @@ package practice_exercises_cop2210;
  * @author Carlos
  */
 public class practice_exercise1_8 {
-    public class
+    public static void main (String[] args)
     
+    //Random creation of my unimaginative mind 
+    
+    System.out.println("----|----|----|-----");
+    System.out.println("--words-into-abyss--");
+    System.out.println("!!##!!##!!##!!##!!@@");
+    System.out.println("//||//||//||//||//||");
+    System.out.println("||//\\||//\\||//\\||");
+    System.out.println("||\\??||\\//||\\//||");
+    System.out.println("@???????????????????");
+    System.out.println("01001110010100100001");
+    System.out.println("____________________");
+    
+    // La fin 
+    
+}
 }
